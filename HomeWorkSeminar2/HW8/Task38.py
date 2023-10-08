d39213d998cabf15d8b3fc8dfa2d6799b8e3914f
«Julia» <>
2023-10-08 19:15:51 +0500

    task38+
    Signed-off-by: «Julia» <ваша>

--- HomeWorkSeminar2/HW8/Task38.py
+++ HomeWorkSeminar2/HW8/Task38.py
@@ -4,8 +4,7 @@
 и Вы должны реализовать функционал для изменения и удаления данных.
 
 """
-
-
+import csv
 from csv import DictWriter, DictReader
 from os.path import exists
 
@@ -13,10 +12,32 @@ def create_file():
     with open('phone.csv', 'w', encoding='utf-8', newline='') as data:
         f_writer = DictWriter(data, fieldnames=['Фамилия', 'Имя', 'Номер'])
         f_writer.writeheader()
-
+"""
 def get_info():
     mas_info = ['Иванов', 'Иван', 123]
     return mas_info
+"""
+
+def get_info():
+    info = []
+    first_name = input('Введите фамилию: ')
+    last_name = input('Введите имя: ')
+    info.append(first_name)
+    info.append(last_name)
+    flag = False
+    while not flag:
+        try:
+            phone_number = int(input('Введите номер телефона: '))
+            if len(str(phone_number)) != 11:
+                print('wrong number')
+            else:
+                flag = True
+
+        except ValueError:
+            print('not valid number')
+        info.append(phone_number)
+        return info
+
 
 def write_file(lst):
     with open('phone.csv', 'r', encoding='utf-8',) as f_n:
@@ -35,6 +56,47 @@ def read_file(file_name):
         f_reader = DictReader(data)
         res = list(f_reader)
     return res
+def change_info(file_name):
+    with open(file_name, 'r+', encoding='utf-8') as f:
+        f_reader = DictReader(f)
+        res = list(f_reader)
+        res_enum = list(enumerate(res))
+        print(res_enum)
+
+        for i, dict in (res_enum):
+            number = int(input('Введите номер контакта для изменения: '))
+            if i == number:
+                print(i, dict)
+                el_1 = input('Введите новую фамилию: ')
+                dict['Фамилия'] = el_1
+                el_2 = input('Введите новое имя: ')
+                dict['Имя'] = el_2
+                el_3 = input('Введите новый номер телефона: ')
+                dict['Номер'] = el_3
+
+                with open(file_name, 'w', encoding='utf-8', newline='') as f:
+                    f_n_writer = DictWriter(f, fieldnames=['Фамилия', 'Имя', 'Номер'])
+                    f_n_writer.writeheader()
+                    f_n_writer.writerows(res)
+                print('Контакт успешно изменен')
+                break
+
+def del_info(file_name):
+    with open(file_name, 'r+', encoding='utf-8') as f:
+        f_reader = DictReader(f)
+        res = list(f_reader)
+        res_enum = list(enumerate(res))
+        print(res_enum)
+        number = int(input('Введите номер контакта для удаления: '))
+        res.pop(number)
+        #print(res)
+        print('Контакт успешно удален')
+
+        with open(file_name, 'w', encoding='utf-8',newline='') as f:
+            f_n_writer = DictWriter(f, fieldnames=['Фамилия', 'Имя', 'Номер'])
+            f_n_writer.writeheader()
+            f_n_writer.writerows(res)
+
 
 def main():
     while True:
@@ -49,6 +111,10 @@ def main():
             if not exists('phone.csv'):
                 create_file()
             write_file(get_info())
+        elif command == 'd':
+            del_info('phone.csv')
+        elif command == 'c':
+            change_info('phone.csv')
 
 
 main()
